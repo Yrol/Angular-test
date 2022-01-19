ecbd92eb7ae981f47c55ce27bb63c176345e3a41
Yrol <yrollgayanth@yahoo.com>
2022-01-19 23:12:53 +1100

    adding the directive *ngFor

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,13 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  //using "{{}}" is known as string interpolation.
-  template:
-    '<h2>{{"Course category: " + getTitle() + "(" + courseCount + ")"}}</h2>',
+  /**
+   * Interpolation - using "{{}}" is known as string interpolation.
+   */
+  // template:
+  //   '<h2>{{"Course category: " + getTitle() + "(" + courseCount + ")"}}</h2>',
+
+  /**
+   * using Directives
+   */
+  template: `
+    <h2>{{ title }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `,
 })
 export class CoursesComponent {
   title = 'Mathematics';
   courseCount = '34';
+  courses = ['courses1', 'courses2', 'courses3'];
 
   getTitle() {
     return this.title;
